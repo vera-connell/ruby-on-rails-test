06573f6594f726e2fbf05d9bc5869947c51b2177
Finn Connell <finn.bc@protonmail.com>
2023-06-29 15:04:09 +1200

    configures a database I think

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+Movie.create(movie: "over the hedge", character: "AJ")
+Movie.create(movie: "it suxx", character: "blah")
+Movie.create(movie: "over the edge", character: "me")
+Movie.create(movie: "my movie", character: "suh")
+Movie.create(movie: "oscar the movie", character: "Oscar")
+Movie.create(movie: "over", character: "thehedge")
+Movie.create(movie: "hgrghirjg", character: "roflmao")
