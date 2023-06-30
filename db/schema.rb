363c8298c4750ddf8438bcbd8aa169a6c2bc998b
Finn Connell <finn.bc@protonmail.com>
2023-06-30 14:40:36 +1200

    adds characters table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_29_025329) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_30_023443) do
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "movies", force: :cascade do |t|
     t.string "movie"
     t.string "character"
